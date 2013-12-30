ae7cf329ad8163e9831866bc563706ee1249e686
Trevor Pilley <trev@trevorpilley.co.uk>
2013-12-30 12:38:09 +0000

    Added solution
    It's a copy of the old MicroLite.Extensions.Log4Net solution

---
+++ MicroLite.Logging.Log4Net/ConfigurationExtensions.cs
@@ -0,0 +1,54 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ConfigurationExtensions.cs" company="MicroLite">
+// Copyright 2012 - 2013 Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Configuration
+{
+    using System;
+    using MicroLite.Logging;
+
+    /// <summary>
+    /// Extensions for the MicroLite configuration.
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures the MicroLite ORM Framework to use log4net as the logging framework.
+        /// </summary>
+        /// <param name="configureExtensions">The interface to configure extensions.</param>
+        /// <returns>The configure extensions.</returns>
+        public static IConfigureExtensions WithLog4Net(this IConfigureExtensions configureExtensions)
+        {
+            if (configureExtensions == null)
+            {
+                throw new ArgumentNullException("configureExtensions");
+            }
+
+            System.Diagnostics.Trace.TraceInformation("MicroLite: loading log4net extension.");
+
+            configureExtensions.SetLogResolver((string name) =>
+            {
+                var logger = log4net.LogManager.GetLogger(name);
+
+                return new LogAdapter(logger);
+            });
+
+            if (!log4net.LogManager.GetRepository().Configured)
+            {
+                System.Diagnostics.Trace.TraceWarning("MicroLite: log4net has not yet been configured, until it has been; no logging will occur.");
+            }
+
+            System.Diagnostics.Trace.TraceInformation("MicroLite: log4net extension loaded.");
+
+            return configureExtensions;
+        }
+    }
+}
